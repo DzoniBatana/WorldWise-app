3f61549dc0067ecbdb0027fe28f3dffcf3fc94e3
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-04-11 22:46:51 +0200

    Update

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,11 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return (
+    <div className={styles.nav}>
+      <nav>App NAvigation</nav>
+    </div>
+  );
+}
+
+export default AppNav;
