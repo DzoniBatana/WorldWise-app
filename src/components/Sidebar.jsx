6a585f3d08f3db89a39270bd1aff9513f4a95106
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-04-14 22:44:28 +0200

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,16 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
+      <Outlet />
+
       <Footer />
-      <p>List of cities</p>
     </div>
   );
 }
