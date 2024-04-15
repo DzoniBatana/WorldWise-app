9d116feab12492b61f8f74d72f7df0f9969a4437
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-04-15 22:36:12 +0200

    update

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,12 +1,5 @@
-import styles from "./CityItem.module.css";
-
-function CityItem({ city }) {
-  console.log(city);
-  return (
-    <li className={styles.CityItem}>
-      <span className={styles.emoji}></span>
-    </li>
-  );
+function CityItem() {
+  return <div></div>;
 }
 
 export default CityItem;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,16 +1,7 @@
 import styles from "./CityList.module.css";
-import Spinner from "./Spinner";
-import CityItem from "./CityItem";
 
-function CityList({ cities, isLoading }) {
-  if (isLoading) return <Spinner />;
-  return (
-    <ul className={styles.cityList}>
-      {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
-      ))}
-    </ul>
-  );
+function CityList() {
+  return <ul className={styles.cityList}>LIST</ul>;
 }
 
 export default CityList;
