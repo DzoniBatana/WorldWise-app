7fe3a9f71b0dc967d61db200afe8dde21b6b1283
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-04-17 23:08:40 +0200

    Update :(

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -37,16 +38,19 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route
             index
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
+
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
