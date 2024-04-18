76606fbdc7b06e74fb12c05e0abece71e7cf5a8e
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-04-18 22:41:53 +0200

    Update navigate route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { Navigate } from "react-router-dom";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -42,10 +43,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
 
           <Route
             path="cities"
